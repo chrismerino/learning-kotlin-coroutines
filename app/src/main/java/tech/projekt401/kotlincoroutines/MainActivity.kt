f3781868696ff8b22a0b257cbeefa57e118de8ac
Chris Merino <10505127+chrismerino@users.noreply.github.com>
2020-01-10 13:15:14 -0600

    Adding functionality to update TextView on MainActivity

--- app/src/main/java/tech/projekt401/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/tech/projekt401/kotlincoroutines/MainActivity.kt
@@ -3,9 +3,16 @@ package tech.projekt401.kotlincoroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "Result #1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,17 +21,41 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
 
-
+            // IO = Local DB or Network interaction
+            // Main = For doing stuff on the main thread, interacting with the UI
+            // Default = Heavy computational work
+            CoroutineScope(IO).launch {
+                fakeApiRequest()
+            }
 
         }
 
     }
 
+    private fun setNewText(input: String){
 
-    private suspend fun getResult1FromApi(){
+        val newText = textView.text.toString() + "\n$input"
+        textView.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(input: String){
+        withContext(Main){
+            setNewText(input)
+        }
+    }
+
+
+    private suspend fun fakeApiRequest(){
+        val result1 = getResult1FromApi()
+        println("debug: $result1")
+        setTextOnMainThread(result1)
+    }
+
+    private suspend fun getResult1FromApi(): String{
         val name = "getResult1FromApi"
         logThread(name)
-        delay(1000)
+        delay(2500)
+        return RESULT_1
     }
 
 
