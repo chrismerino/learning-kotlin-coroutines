0247136930dae533559adf79143653cc9c62f8b0
Chris Merino <10505127+chrismerino@users.noreply.github.com>
2020-02-19 05:49:20 -0600

    Update MainActivity.kt
    Una funcion suspend solo puede iniciarse desde el Scope de un Coroutine

--- app/src/main/java/tech/projekt401/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/tech/projekt401/kotlincoroutines/MainActivity.kt
@@ -21,9 +21,9 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
 
-            // IO = Local DB or Network interaction
-            // Main = For doing stuff on the main thread, interacting with the UI
-            // Default = Heavy computational work
+            // IO = Para llamadas a la red (Retrofit o Volley) o para llamadas a una base local (Room)
+            // Main = Para hacer cosas en el Hilo principal, como interactuar con el UI
+            // Default = Para trabajo computacional que requiera bastantes recursos. Como renderizado.
             CoroutineScope(IO).launch {
                 fakeApiRequest()
             }
@@ -31,26 +31,39 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
+    
+    // Una vez obtenido el valor y ya asegurando que esta disponible en Main Scope, ya es
+    // manipulable y el resultado puede mostrarse u ocuparse donde sea necesario
+    // Siempre y cuando sea necesario que se ocupe en el Main Scope.
     private fun setNewText(input: String){
 
         val newText = textView.text.toString() + "\n$input"
         textView.text = newText
     }
 
+    // En este metodo se cambia el Scope, que inicialmente era IO desde donde se inicio la
+    // Coroutine y se cambia al Main Scope para que el resultado pueda ser manipulado y
+    // mostrado en un TextView que se encuentra en el Main Scope.
+    
+   // Si no se cambia el Scope antes de tratar de asignar el valor y mostrarlo en el TextView
+    // La app se cerrara debido a que el valor no es accesible desde IO.
     private suspend fun setTextOnMainThread(input: String){
         withContext(Main){
             setNewText(input)
         }
     }
 
-
+    // En este metodo se declara una variable cuyo valor se obtiene desde una llamada
+    // a la API, una vez se obtenga el valor entonces se mandara ese valor desde el Score
+    // IO (que es desde donde se inicio la Coroutine y se pasa al contexto Main para que
+    // la app pueda mostrar el valor en una pantalla (en el Scope Main)
     private suspend fun fakeApiRequest(){
         val result1 = getResult1FromApi()
         println("debug: $result1")
         setTextOnMainThread(result1)
     }
 
+    // Este metodo simula una llamada a una API que tarda 2500ms en responder.
     private suspend fun getResult1FromApi(): String{
         val name = "getResult1FromApi"
         logThread(name)
@@ -59,6 +72,8 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    // Este metodo sirve unicamente para propositos de "logear" el Hilo (Thread)
+    // Donde se esta ejecutando la Coroutine.
     private fun logThread(methodName: String){
         println("debug ${methodName}: ${Thread.currentThread().name}")
     }
