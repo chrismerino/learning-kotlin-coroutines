832ee448a13f1ea13ef59479c7ef09182bb7982e
Cristian Merino <cris.merino@live.com>
2020-02-23 21:38:20 -0600

    Adding some more code

--- app/src/main/java/tech/projekt401/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/tech/projekt401/kotlincoroutines/MainActivity.kt
@@ -12,28 +12,24 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
+    private val RESULT_2 = "Result #2"
     private val RESULT_1 = "Result #1"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         button.setOnClickListener {
-
             // IO = Local DB or Network interaction
             // Main = For doing stuff on the main thread, interacting with the UI
             // Default = Heavy computational work
             CoroutineScope(IO).launch {
                 fakeApiRequest()
             }
-
         }
-
     }
 
     private fun setNewText(input: String){
-
         val newText = textView.text.toString() + "\n$input"
         textView.text = newText
     }
@@ -44,23 +40,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private suspend fun fakeApiRequest(){
         val result1 = getResult1FromApi()
         println("debug: $result1")
         setTextOnMainThread(result1)
+
+        val result2 = getResult2FromApi(result1)
+        setTextOnMainThread(result2)
     }
 
     private suspend fun getResult1FromApi(): String{
         val name = "getResult1FromApi"
         logThread(name)
-        delay(2500)
+        delay(1000)
         return RESULT_1
     }
 
+    private suspend fun getResult2FromApi(result1: String): String {
+        logThread("getResult2fromApi")
+        delay(1000)
+        return RESULT_2
+    }
 
     private fun logThread(methodName: String){
         println("debug ${methodName}: ${Thread.currentThread().name}")
     }
-
 }
